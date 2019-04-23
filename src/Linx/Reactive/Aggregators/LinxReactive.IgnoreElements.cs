48574c7b4d629b38ff7ae6618159ff3ec0d81ad2
TINUSPC\Martin <martin.durtschi@netiva.ch>
2019-04-23 06:05:18 +0200

    no message

---
+++ src/Linx/Reactive/Aggregators/LinxReactive.IgnoreElements.cs
@@ -0,0 +1,22 @@
+﻿namespace Linx.Reactive
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    partial class LinxReactive
+    {
+        /// <summary>
+        /// Consumes <paramref name="source"/> ignoring its elements.
+        /// </summary>
+        public static async Task IgnoreElements<T>(this IAsyncEnumerable<T> source, CancellationToken token)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            token.ThrowIfCancellationRequested();
+            var ae = source.GetAsyncEnumerator(token);
+            try { while (await ae.MoveNextAsync()) { } }
+            finally { await ae.DisposeAsync().ConfigureAwait(false); }
+        }
+    }
+}
