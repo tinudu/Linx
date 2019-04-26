e93e7505008ef92681439eafa0fec2e83a147bc4
Martin Durtschi <martin.durtschi@netiva.com>
2019-04-26 09:05:39 +0200

    Switch

--- src/Linx/Reactive/Operators/LinxReactive.Switch.cs
+++ src/Linx/Reactive/Operators/LinxReactive.Switch.cs
@@ -1,6 +1,7 @@
 ﻿namespace Linx.Reactive
 {
     using System;
+    using System.Threading;
 
     partial class LinxReactive
     {
@@ -10,7 +11,45 @@
         public static IAsyncEnumerable<T> Switch<T>(this IAsyncEnumerable<IAsyncEnumerable<T>> sources)
         {
             if (sources == null) throw new ArgumentNullException(nameof(sources));
-            throw new NotImplementedException();
+
+            return Produce<T>(async (yield, token) =>
+            {
+                CancellationTokenSource ctsInner = null;
+
+                void CancelInner()
+                {
+                    // ReSharper disable once AccessToModifiedClosure
+                    var cts = Interlocked.Exchange(ref ctsInner, null);
+                    if (cts == null) return;
+                    try { cts.Cancel(); } catch { /**/ }
+                }
+
+                var aeOuter = sources
+                    .Do(v => CancelInner(), e => CancelInner())
+                    .Latest()
+                    .GetAsyncEnumerator(token);
+                try
+                {
+                    while (await aeOuter.MoveNextAsync())
+                    {
+                        var cts = new CancellationTokenSource();
+                        ctsInner = cts;
+                        try
+                        {
+                            var aeInner = aeOuter.Current.GetAsyncEnumerator(cts.Token);
+                            try
+                            {
+                                while (await aeInner.MoveNextAsync())
+                                    await yield(aeInner.Current);
+                            }
+                            finally { await aeInner.DisposeAsync().ConfigureAwait(false); }
+                        }
+                        catch (OperationCanceledException oce) when (oce.CancellationToken == cts.Token) { }
+                        finally { CancelInner(); }
+                    }
+                }
+                finally { await aeOuter.DisposeAsync().ConfigureAwait(false); }
+            });
         }
     }
 }
