07bc2f57872666862d86d618e2341ebdd4058dbd
Martin Durtschi <martin.durtschi@netiva.com>
2019-04-26 10:22:03 +0200

    Catch, Finally

---
+++ src/Linx/Reactive/Operators/LinxReactive.Catch.cs
@@ -0,0 +1,28 @@
+﻿namespace Linx.Reactive
+{
+    using System;
+
+    partial class LinxReactive
+    {
+        /// <summary>
+        /// Calls the specified <paramref name="handler"/> if an error of type <typeparamref name="TException"/> occurs.
+        /// </summary>
+        public static IAsyncEnumerable<TSource> Catch<TSource, TException>(this IAsyncEnumerable<TSource> source, Action<TException> handler) where TException : Exception
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (handler == null) throw new ArgumentNullException(nameof(handler));
+
+            return Produce<TSource>(async (yield, token) =>
+            {
+                var ae = source.GetAsyncEnumerator(token);
+                try
+                {
+                    while (await ae.MoveNextAsync())
+                        await yield(ae.Current);
+                }
+                catch (TException ex) { handler(ex); }
+                finally { await ae.DisposeAsync().ConfigureAwait(false); }
+            });
+        }
+    }
+}
---
+++ src/Linx/Reactive/Operators/LinxReactive.Finally.cs
@@ -0,0 +1,29 @@
+﻿namespace Linx.Reactive
+{
+    using System;
+
+    partial class LinxReactive
+    {
+        public static IAsyncEnumerable<T> Finally<T>(this IAsyncEnumerable<T> source, Action @finally)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (@finally == null) throw new ArgumentNullException(nameof(@finally));
+
+            return Produce<T>(async (yield, token) =>
+            {
+                var ae = source.GetAsyncEnumerator(token);
+                try
+                {
+                    while (await ae.MoveNextAsync())
+                        await yield(ae.Current);
+                }
+                finally
+                {
+                    await ae.DisposeAsync().ConfigureAwait(false);
+                    @finally();
+                }
+            });
+
+        }
+    }
+}
