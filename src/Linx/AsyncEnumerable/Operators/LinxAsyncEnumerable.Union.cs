eba5483af60efe6685c48aaa74bf180e94525884
Martin Durtschi <martin.durtschi@netiva.com>
2019-06-06 19:49:44 +0200

    .

--- src/Linx/AsyncEnumerable/Operators/LinxAsyncEnumerable.Union.cs
+++ src/Linx/AsyncEnumerable/Operators/LinxAsyncEnumerable.Union.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Linx.AsyncEnumerable.Operators
+﻿namespace Linx.AsyncEnumerable
 {
-    class LinxAsyncEnumerable
+    using System.Collections.Generic;
+
+    partial class LinxAsyncEnumerable
     {
+        /// <summary>
+        /// Produces the set union of two sequences.
+        /// </summary>
+        public static IAsyncEnumerable<T> Union<T>(this IAsyncEnumerable<T> first, IAsyncEnumerable<T> second, IEqualityComparer<T> comparer = null)
+            => first.Merge(second).Distinct(comparer);
     }
 }
