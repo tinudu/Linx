da2d112dc0dc3bfa833d49292ca7489be9db2ef9
TINUSPC\Martin <martin.durtschi@netiva.ch>
2019-08-01 23:41:24 +0200

    no message

--- src/Linx/AsyncEnumerable/Operators/LinxAsyncEnumerable.TimeInterval.cs
+++ src/Linx/AsyncEnumerable/Operators/LinxAsyncEnumerable.TimeInterval.cs
@@ -1,13 +1,35 @@
 ﻿namespace Linx.AsyncEnumerable
 {
+    using System;
     using System.Collections.Generic;
-    using Observable;
+    using System.Threading.Tasks;
+    using Timing;
 
     partial class LinxAsyncEnumerable
     {
         /// <summary>
         /// Records the time interval between consecutive values.
         /// </summary>
-        public static ILinxObservable<TimeInterval<T>> TimeInterval<T>(this IAsyncEnumerable<T> source) => source.ToLinxObservable().TimeInterval();
+        public static IAsyncEnumerable<TimeInterval<T>> TimeInterval<T>(this IAsyncEnumerable<T> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            return Create<TimeInterval<T>>(async (yield, token) =>
+            {
+                var time = Time.Current;
+                var t0 = time.Now;
+                var ae = source.WithCancellation(token).ConfigureAwait(false).GetAsyncEnumerator();
+                try
+                {
+                    while (await ae.MoveNextAsync())
+                    {
+                        var t = time.Now;
+                        if (!await yield(new TimeInterval<T>(t - t0, ae.Current)).ConfigureAwait(false))
+                            return;
+                        t0 = t;
+                    }
+                }
+                finally { await ae.DisposeAsync(); }
+            });
+        }
     }
 }
